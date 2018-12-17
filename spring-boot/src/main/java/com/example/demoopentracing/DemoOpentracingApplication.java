a5f7d1e0baf47ee286c475f6c899455454d539ac
Edward Tsang <etsang@splunk.com>
2018-12-17 15:46:40 -0800

    add dummy func to register span decorators

--- spring-boot/src/main/java/com/example/demoopentracing/DemoOpentracingApplication.java
+++ spring-boot/src/main/java/com/example/demoopentracing/DemoOpentracingApplication.java
@@ -1,5 +1,7 @@
 package com.example.demoopentracing;
 
+import com.example.demoopentracing.interceptor.RequestSpanDecorator;
+import io.opentracing.contrib.spring.web.interceptor.HandlerInterceptorSpanDecorator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +11,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import com.splunk.observation.tracing.LightstepTracer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoOpentracingApplication {
@@ -20,6 +25,18 @@ public class DemoOpentracingApplication {
 	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
 	    RestTemplate restTemplate = restTemplateBuilder.build();
 	    return restTemplate;
+	}
+
+
+	@Bean
+	public List<HandlerInterceptorSpanDecorator> spanDecorators() {
+		LOGGER.info("This may be the place to register our own span decorators to fix the span");
+		// We may not need Any of those tracing interceptors.
+		// * https://github.com/opentracing-contrib/java-spring-web/blob/0933caece7de11ae9f9d411113a5473d35288cf8/opentracing-spring-web/src/main/java/io/opentracing/contrib/spring/web/interceptor/TracingHandlerInterceptor.java#L41
+		//
+		// *  https://github.com/opentracing-contrib/java-spring-web/tree/0933caece7de11ae9f9d411113a5473d35288cf8/opentracing-spring-web/src/main/java/io/opentracing/contrib/spring/web/interceptor
+		//
+		return new ArrayList<>();
 	}
 
 	@Bean
